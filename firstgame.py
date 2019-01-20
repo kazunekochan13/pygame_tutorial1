d4df73cba235e73c4ebb60642b0ec79b478391a7
KazuNekoCHAN13 <kazuneko78d@gmail.com>
2019-01-20 14:50:40 +0800

    projectiles

--- firstgame.py
+++ firstgame.py
@@ -30,30 +30,51 @@ class player(object):
 		self.left = False
 		self.right = False
 		self.walkCount = 0
+		self.standing = True
 
 	def draw(self, win):
-		if self.walkCount + 1 >= 27:
+		if (self.walkCount + 1) >= 27:
 			self.walkCount = 0
-		if self.left:
-			win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
-			self.walkCount += 1
-		elif self.right:
-			win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
-			self.walkCount += 1
+			if not(self.standing):
+				if self.left:
+					win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
+					self.walkCount += 1
+				elif self.right:
+					win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
+					self.walkCount += 1
 		else:
-			win.blit(char, (self.x,self.y))
+			if self.right:
+				win.blit(walkRight[0], (self.x, self.y))
+			else:
+				win.blit(walkLeft[0], (self.x, self.y))
+
+class projectile(object):
+
+	def __init__(self, x, y, radius, color, facing):
+		self.x = x
+		self.y = y
+		self.radius = radius
+		self.color = color
+		self.facing = facing
+		self.vel = 8 * facing
+
+	def draw(self, win):
+		pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
 def redrawGameWindow():
 	global walkCount # uses the walkCount variable from outside the scope of this function instead of instantiating a new one
 	win.blit(bg, (0,0))
 	
 	man.draw(win)
+	for bullet in bullets:
+		bullet.draw(win)
 
 	pygame.display.update() # refresh window
 
 
 # main loop
 man = player(300, 410, 64, 64)
+bullets = []
 run = True
 while run:
 
@@ -63,21 +84,37 @@ while run:
 		if event.type == pygame.QUIT:
 			run = False
 
+	for bullet in bullets:
+		if bullet.x < screen_x and bullet.x > 0:
+			bullet.x += bullet.vel
+		else:
+			bullets.pop(bullets.index(bullet))
+
 	keys = pygame.key.get_pressed()
+
+	if keys[pygame.K_SPACE]:
+		if man.left:
+			facing = (-1)
+		else:
+			facing = 1
+		if len(bullets) < 5:
+			bullets.append(projectile(round(man.x + man.width // 2), round(man.y + man.height // 2), 6, (0, 0, 0), facing))
 
 	if keys[pygame.K_LEFT] and man.x > 0:
 		man.x -= man.vel
 		man.left = True
 		man.right = False
+		man.standing = False
 	elif keys[pygame.K_RIGHT] and man.x < (screen_x - man.width):
 		man.x += man.vel
 		man.right = True
 		man.left = False
+		man.standing = False
 	else:
-		man.left = man.right = False
+		man.standing = True
 		man.walkCount = 0
 	if not(man.isjump):
-		if keys[pygame.K_SPACE]:
+		if keys[pygame.K_UP]:
 			man.isjump = True
 			man.left = False
 			man.right = False
