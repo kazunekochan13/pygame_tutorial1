0d021a5fe6e442edad09122b34d394fb09708097
KazuNekoCHAN13 <kazuneko78d@gmail.com>
2019-01-20 10:57:18 +0800

    jumping and boundaries

--- firstgame.py
+++ firstgame.py
@@ -1,15 +1,22 @@
+# following tutorial by Tech With Tim @ youtube.com
+
 import pygame
 pygame.init() # always need this line in beginning of program to prevent error running
 
-win = pygame.display.set_mode((500, 500)) # creates a window and sets its dimensions
+screen_x = 500
+screen_y = 500
+win = pygame.display.set_mode((screen_x, screen_y)) # creates a window and sets its dimensions
 
 pygame.display.set_caption("First Game")
 
 x = 50
-y = 50
+y = 450
 width = 40
 height = 60
 vel = 5
+
+isjump = True
+jumpCount = 10
 
 run = True
 while run:
@@ -21,14 +28,27 @@ while run:
 
 	keys = pygame.key.get_pressed()
 
-	if keys[pygame.K_LEFT]:
+	if keys[pygame.K_LEFT] and x > 0:
 		x -= vel
-	if keys[pygame.K_RIGHT]:
+	if keys[pygame.K_RIGHT] and x < (screen_x - width):
 		x += vel
-	if keys[pygame.K_UP]:
-		y -= vel
-	if keys[pygame.K_DOWN]:
-		y += vel
+	if not(isjump):
+		if keys[pygame.K_UP] and y > 0:
+			y -= vel
+		if keys[pygame.K_DOWN] and y < (screen_y - height):
+			y += vel
+		if keys[pygame.K_SPACE]:
+			isjump = True
+	else:
+		if jumpCount >= (-10):
+			neg = 1
+			if jumpCount < 0:
+				neg = (-1)
+			y -= (jumpCount ** 2) * 0.5 * neg
+			jumpCount -= 1
+		else:
+			isjump = False
+			jumpCount = 10
 
 	win.fill((0, 0, 0))
 	pygame.draw.rect(win, (255, 0, 0), (x, y, width, height)) # drawing character
